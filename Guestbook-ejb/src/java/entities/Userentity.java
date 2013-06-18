5e431d9607f191bcb89789e1958c0f436d90d621
matthiasvp <matthiasvp@gmail.com>
2013-06-18 19:56:29 +0200

    its Aliiiive

--- Guestbook-ejb/src/java/entities/Userentity.java
+++ Guestbook-ejb/src/java/entities/Userentity.java
@@ -41,7 +41,7 @@ import javax.validation.constraints.Pattern;
 	@NamedQuery(name = "Userentity.findByPassword", query = "SELECT u FROM Userentity u WHERE u.password = :password"),
 	@NamedQuery(name = "Userentity.findByRights", query = "SELECT u FROM Userentity u WHERE u.rights = :rights"),
 	@NamedQuery(name = "Userentity.findBirthdays", query = "SELECT u FROM Userentity u WHERE FUNC('MONTH', u.dateofbirth) = :month AND FUNC('DAY', u.dateofbirth) = :day"),
-	@NamedQuery(name= "Userentity.loginCheck", query = "SELECT u from Userentity u WHERE u.username = :username AND u.password = :password")
+	@NamedQuery(name= "Userentity.checkLogin", query = "SELECT u from Userentity u WHERE u.username = :username AND u.password = :password")
 })
 public class Userentity implements Serializable {
 	private static final long serialVersionUID = 1L;
