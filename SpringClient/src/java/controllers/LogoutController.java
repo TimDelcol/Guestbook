9a097d6e785bc113babf2a2b52237912a3df8a30
silverslide <roelstorms@hotmail.com>
2013-06-20 13:19:56 +0200

    Logout

---
+++ SpringClient/src/java/controllers/LogoutController.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import beans.AccessBeanRemote;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Roel_Storms
+ */
+
+@Controller
+public class LogoutController {
+    AccessBeanRemote accessBean = lookupAccessBeanRemote();
+    
+    @RequestMapping(value = "/logout.htm")
+    public String showForm(Map model) {
+        System.out.println("logout.htm ");
+        if(accessBean.isLoggedIn())
+        {
+            model.put("loggedIn", "<font color='green'>Succesfully logged out</font>");
+            accessBean.logout();
+            return "login";
+        }
+        model.put("loggedIn", "<font color='red'>You were not logged in</font>");
+        return "login";
+    }
+    
+      private AccessBeanRemote lookupAccessBeanRemote() {
+        try {
+            Context c = new InitialContext();
+            return (AccessBeanRemote) c.lookup("java:global/Guestbook/Guestbook-ejb/AccessBean!beans.AccessBeanRemote");
+        } catch (NamingException ne) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
+            throw new RuntimeException(ne);
+        }
+    }
+}
